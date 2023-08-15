1ba6be3f6a93dc0d7d678f481c0df0d1c3e328c3
mayankdubey0 <108892930+mayankdubey0@users.noreply.github.com>
2023-08-14 22:21:35 -0500

    Update Throttle.ino

--- Throttle.ino
+++ Throttle.ino
@@ -1,10 +1,11 @@
+#include <Servo.h>
 #include "ppm.h"
 
 // PPM channel layout (update for your situation)
-#define THROTTLE        2
-#define ROLL            4
-#define PITCH           3
-#define YAW             1
+#define THROTTLE        3
+#define ROLL            1
+#define PITCH           2
+#define YAW             4
 #define SWITCH3WAY_1    5
 #define BUTTON          6
 #define SWITCH3WAY_2    7     // trim-pot for left/right motor mix  (face trim)
@@ -14,19 +15,26 @@
 const long interval = 50;
 unsigned long previousMillis = 0;
 
+bool callib = true;
+
+Servo ESC1;
+Servo ESC2;
+
 void setup()
 {
   // Start the serial port to display data 
   Serial.begin(115200);
+  ESC1.attach(9, 1000, 2000);
+  ESC2.attach(10, 1000, 2000);
+
+  ppm.begin(A0, false);
 
   // Start the PPM function on PIN A0
-  ppm.begin(A0, false);
-  pinMode(13, OUTPUT);
+
 }
 
 void loop()
 {
-  // Interval at which the PPM is updated
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
@@ -37,12 +45,11 @@ void loop()
     short pitch         =   (-ppm.read_channel(PITCH)+1500)/2;
     short yaw           =   (-ppm.read_channel(YAW)+1500)/2;
 
-    // Print the values for the Arduino Serial Plotter
+    ESC1.write(throttle);
+    ESC2.write(throttle);
     Serial.print("Throttle:");        Serial.print(throttle);       Serial.print(" ");
     Serial.print("Roll:");            Serial.print(roll);           Serial.print(" ");
     Serial.print("Pitch:");           Serial.print(pitch);          Serial.print(" ");
-    Serial.print("Yaw:");             Serial.print(yaw);            Serial.print(" ");
-  
-    Serial.println();
+    Serial.print("Yaw:");             Serial.print(yaw);            Serial.print("\n");
   }
 }
